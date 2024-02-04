774ca5a73f405befef7b7d5ba1fe0e6f826954b7
willian-vieira <williannvieira.w@gmail.com>
2024-02-04 12:32:03 -0300

    feat: Criado CategoryDTO

---
+++ src/main/java/productapi/dto/CategoryDTO.java
@@ -0,0 +1,25 @@
+package productapi.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class CategoryDTO {
+    @NotNull
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

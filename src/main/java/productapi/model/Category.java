da46e5623e53f1601c7c5ebb0669e13004cc2cd5
willian-vieira <williannvieira.w@gmail.com>
2024-02-04 11:51:55 -0300

    Classes Product e Category criadas e Persistidas no Banco - (JPA)

---
+++ src/main/java/productapi/model/Category.java
@@ -0,0 +1,28 @@
+package productapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/productapi/model/Product.java
@@ -0,0 +1,69 @@
+package productapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Float price;
+    @Column(length = 150, nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String productIdentifier;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProductIdentifier() {
+        return productIdentifier;
+    }
+
+    public void setProductIdentifier(String productIdentifier) {
+        this.productIdentifier = productIdentifier;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}

0148e36ff7ce8256e1286ffddc0f54682455c72f
willian-vieira <williannvieira.w@gmail.com>
2024-08-10 15:15:08 -0300

    feat: Classe Product Criada

---
+++ src/main/java/com/api_product/entities/Product.java
@@ -0,0 +1,26 @@
+package com.api_product.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Float price;
+    @Column(length = 120, nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String productIdentifier;
+}
